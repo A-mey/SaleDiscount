78e06d4f97506f25b2e81f4cb978862ca82c2914
Ameya <amey2p@gmail.com>
2024-08-26 00:17:02 +0530

    added catch block

--- sale_discount.js
+++ sale_discount.js
@@ -4,10 +4,14 @@ import { ItemController } from './items/controllers/item.controller.js';
 import { ItemService } from './items/services/item.service.js';
 
 const main = async () => {
-  const itemService = new ItemService();
-  const itemController = new ItemController(itemService);
-  const response = await itemController.checkPriceAndDiscount();
-  console.log(response);
+  try {
+    const itemService = new ItemService();
+    const itemController = new ItemController(itemService);
+    const response = await itemController.checkPriceAndDiscount();
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 main();
